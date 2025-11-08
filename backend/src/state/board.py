98b96e840afb8e8186501c15745f45b38f36b472
Ramleton <77259553+Ramleton@users.noreply.github.com>
2025-11-08 13:57:50 -0500

    Added clone and restore methods to Board class

--- backend/src/state/board.py
+++ backend/src/state/board.py
@@ -11,6 +11,9 @@ class Cell(BaseModel):
 	colour: str
 	state: CellState
 
+	def __init__(self,  colour: str, state: CellState):
+		super().__init__(colour=colour, state=state)
+
 	def __str__(self):
 		letter = " " if self.state == CellState.EMPTY else "X" if self.state == CellState.MARKED else "Q"
 		return "[{}]".format(letter)
@@ -22,6 +25,15 @@ class Board(BaseModel):
 
 	def __init__(self, rows: int, cols: int, grid: List[List[Cell]]):
 		super().__init__(rows=rows, cols=cols, grid=grid)
+	
+	def clone(self) -> "Board":
+		new_grid = [[Cell(cell.colour, cell.state) for cell in row] for row in self.grid]
+		return Board(rows=self.rows, cols=self.cols, grid=new_grid)
+
+	def restore_from(self, other: "Board"):
+		for r in range(self.rows):
+			for c in range(self.cols):
+				self.grid[r][c] = other.grid[r][c]
 	
 	def print(self) -> None:
 		for row in self.grid:
