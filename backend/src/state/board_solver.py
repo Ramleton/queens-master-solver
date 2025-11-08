55e8b9fdc792163c40d3102d35b70f418c814e00
Ramleton <77259553+Ramleton@users.noreply.github.com>
2025-11-08 01:36:44 -0500

    Handle over-constrained color groups in columns via row/column comparison

--- backend/src/state/board_solver.py
+++ backend/src/state/board_solver.py
@@ -5,22 +5,35 @@ class BoardSolver:
 	board: Board
 	unmarked_colour_dict: dict[str, list[tuple[int, int]]]
 	colours_queen_dict: dict[str, bool]
+	colours_to_rc: dict[str, tuple[dict[int, int], dict[int, int]]]
 
 	def __init__(self, board: Board) -> None:
 		self.board = board
 		self.unmarked_colour_dict = {}
 		self.colours_queen_dict = {}
+		self.colours_to_rc = {}
 		for row in range(self.board.rows):
 			for col in range(self.board.cols):
+				# Add all colours to colours_to_rc
+				if self.colours_to_rc.get(self.board.grid[row][col].colour) is None:
+					self.colours_to_rc[self.board.grid[row][col].colour] = (dict(), dict())
 				# Add all colours to colours_queen_dict
 				if self.unmarked_colour_dict.get(self.board.grid[row][col].colour) is None:
 					self.unmarked_colour_dict[self.board.grid[row][col].colour] = []
 				if self.colours_queen_dict.get(self.board.grid[row][col].colour) is None:
 					self.colours_queen_dict[self.board.grid[row][col].colour] = False
-				# Add all unmarked cells to unmarked_colour_dict
+				# Initialize empty rows and columns in colours_to_rc
+				if self.colours_to_rc[self.board.grid[row][col].colour][0].get(row) is None:
+					self.colours_to_rc[self.board.grid[row][col].colour][0][row] = 0
+				if self.colours_to_rc[self.board.grid[row][col].colour][1].get(col) is None:
+					self.colours_to_rc[self.board.grid[row][col].colour][1][col] = 0
+				# Add all unmarked cells to unmarked_colour_dict and colours_to_rc
 				if self.board.grid[row][col].state == CellState.EMPTY:
 					self.unmarked_colour_dict[self.board.grid[row][col].colour]\
 						.append((row, col))
+					self.colours_to_rc[self.board.grid[row][col].colour][0][row] += 1
+					self.colours_to_rc[self.board.grid[row][col].colour][1][col] += 1
+				# Add all queens to colours_queen_dict
 				if self.board.grid[row][col].state == CellState.QUEEN:
 					self.colours_queen_dict[self.board.grid[row][col].colour] = True
 	
@@ -40,6 +53,8 @@ class BoardSolver:
 		try:
 			if self.unmarked_colour_dict.get(self.board.grid[row][col].colour):
 				self.unmarked_colour_dict[self.board.grid[row][col].colour].remove((row, col))
+			self.colours_to_rc[self.board.grid[row][col].colour][0][row] -= 1
+			self.colours_to_rc[self.board.grid[row][col].colour][1][col] -= 1
 		except ValueError:
 			pass
 		
@@ -149,9 +164,6 @@ class BoardSolver:
 			for col in range(self.board.cols):
 				if self._check_cell_empty(row, col):
 					self._check_queen_would_conflict(row, col)
-		# (row, col) = self._flatten(list(self.unmarked_colour_dict.values())).pop()
-		# # print(f"Checking {row}, {col}")
-		# self._check_queen_would_conflict(row, col)
 	
 	def _check_colour_row_helper(self, colour):
 		row = None
@@ -190,12 +202,77 @@ class BoardSolver:
 					self._check_cell_empty(row, column) and \
 					self.board.grid[row][column].colour != colour:
 					self._mark_cell_as_marked(row, column)
+	
+	def _sort_by_least(self):
+		return sorted(self.unmarked_colour_dict, key=lambda colour: len(self.unmarked_colour_dict[colour]))
+	
+	def _find_columns_to_compare(self, colour):
+		columns_to_check = set()
+		for (column, number) in self.colours_to_rc[colour][1].items():
+			if number:
+				columns_to_check.add(column)
+		return columns_to_check
+
+	def _find_rows_to_compare(self, colour):
+		rows_to_check = set()
+		for (row, number) in self.colours_to_rc[colour][0].items():
+			if number:
+				rows_to_check.add(row)
+		return rows_to_check
+	
+	def _check_columns(self):
+		sorted_colours = self._sort_by_least()
+		while sorted_colours:
+			colour = sorted_colours.pop(0)
+			columns_to_check = self._find_columns_to_compare(colour)
+			if not len(columns_to_check):
+				continue
+			num_supersets = 1 # The colour itself is a superset
+			strict_supersets = set()
+			for o_colour in sorted_colours:
+				o_columns_to_check = self._find_columns_to_compare(o_colour)
+				if columns_to_check.issubset(o_columns_to_check):
+					num_supersets += 1
+					if len(o_columns_to_check) > len(columns_to_check):
+						strict_supersets.add(o_colour)
+			if len(strict_supersets) and num_supersets > len(columns_to_check):
+				# There is a strict superset of columns that are not a subset
+				# So, we mark the cells of the colours in the strict supersets with matching columns
+				for o_colour in strict_supersets:
+					for (row, column) in copy.deepcopy(self.unmarked_colour_dict[o_colour]):
+						if column in columns_to_check:
+							self._mark_cell_as_marked(row, column)
+	
+	def _check_rows(self):
+		sorted_colours = self._sort_by_least()
+		while sorted_colours:
+			colour = sorted_colours.pop(0)
+			rows_to_check = self._find_rows_to_compare(colour)
+			if not len(rows_to_check):
+				continue
+			num_supersets = 1 # The colour itself is a superset
+			strict_supersets = set()
+			for o_colour in sorted_colours:
+				o_rows_to_check = self._find_rows_to_compare(o_colour)
+				if rows_to_check.issubset(o_rows_to_check):
+					num_supersets += 1
+					if len(o_rows_to_check) > len(rows_to_check):
+						strict_supersets.add(o_colour)
+			if len(strict_supersets) and num_supersets > len(rows_to_check):
+				# There is a strict superset of rows that are not a subset
+				# So, we mark the cells of the colours in the strict supersets with matching rows
+				for o_colour in strict_supersets:
+					for (row, column) in copy.deepcopy(self.unmarked_colour_dict[o_colour]):
+						if row in rows_to_check:
+							self._mark_cell_as_marked(row, column)
 
 	def _check_steps(self):
 		self._check_single_colour()
 		self._check_colour_row()
 		self._check_colour_column()
 		self._check_cells_iterative()
+		self._check_columns()
+		self._check_rows()
 	
 	def solve(self):
 		self._check_queens()
